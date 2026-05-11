7e89bccb864eed78139d481ce521c2f35f027a65
Nik <sir.nikolai00@mail.ru>
2026-05-11 20:37:19 +0300

    Админки

--- psyhodiary/admin.py
+++ psyhodiary/admin.py
@@ -1,3 +1,39 @@
-# from django.contrib import admin
+from django.contrib import admin
+from .models import Diary, Dream, Habit, HabitLog, TestResult, EmergencyContact
 
-# Register your models here.
+@admin.register(Diary)
+class DiaryAdmin(admin.ModelAdmin):
+    list_display = ('title', 'user', 'mood', 'created_date')
+    list_filter = ('mood', 'created_date')
+    search_fields = ('title', 'content', 'user__email')
+    date_hierarchy = 'created_date'
+
+@admin.register(Dream)
+class DreamAdmin(admin.ModelAdmin):
+    list_display = ('title', 'user', 'status', 'target_date', 'created_at')
+    list_filter = ('status', 'created_at')
+    search_fields = ('title', 'user__email')
+
+@admin.register(Habit)
+class HabitAdmin(admin.ModelAdmin):
+    list_display = ('name', 'user', 'period', 'target_count')
+    list_filter = ('period',)
+    search_fields = ('name', 'user__email')
+
+@admin.register(HabitLog)
+class HabitLogAdmin(admin.ModelAdmin):
+    list_display = ('habit', 'user', 'completed_date')
+    list_filter = ('completed_date',)
+    search_fields = ('habit__name', 'user__email')
+
+@admin.register(TestResult)
+class TestResultAdmin(admin.ModelAdmin):
+    list_display = ('test_name', 'user', 'score', 'created_at')
+    list_filter = ('test_name', 'created_at')
+    search_fields = ('test_name', 'user__email')
+
+@admin.register(EmergencyContact)
+class EmergencyContactAdmin(admin.ModelAdmin):
+    list_display = ('title', 'phone', 'is_24h', 'order')
+    list_editable = ('order',)
+    ordering = ('order',)
--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,28 @@
-# from django.contrib import admin
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser, DoctorProfile
 
-# Register your models here.
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    list_display = ('email', 'name', 'is_doctor', 'is_active', 'date_joined')
+    list_filter = ('is_doctor', 'is_active', 'is_staff')
+    search_fields = ('email', 'name', 'phone_number')
+    ordering = ('-date_joined',)
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Личная информация', {'fields': ('name', 'avatar', 'phone_number', 'description')}),
+        ('Права доступа', {'fields': ('is_doctor', 'is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Медицинские данные', {'fields': ('attending_doctor',)}),
+        ('Важные даты', {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'name', 'password1', 'password2'),
+        }),
+    )
+
+@admin.register(DoctorProfile)
+class DoctorProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'specialization', 'experience_years', 'license_number')
+    search_fields = ('user__email', 'specialization')
